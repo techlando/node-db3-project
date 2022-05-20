58a90e9396ab23e8dd6b08550b51ae3ec89f5273
Landon Phillips <landonphillips@Landons-MacBook-Pro.local>
2022-05-20 11:07:43 -0500

    mvp finished

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -13,7 +13,7 @@ const checkSchemeId = async (req, res, next) => {
       const existing = await db('schemes').where('scheme_id', req.params.scheme_id).first()
       if(!existing) {
         next({ status:404,
-        message: `scheme with schema_id ${req.params.scheme_id}`})
+        message: `scheme with scheme_id ${req.params.scheme_id} not found`})
       } else {
         next()
       }
