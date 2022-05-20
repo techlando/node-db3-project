be33416cfb4e215d453d12d36c5e781d408245d4
Landon Phillips <landonphillips@Landons-MacBook-Pro.local>
2022-05-19 23:44:23 -0500

    second commit

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -31,7 +31,16 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+  const { scheme_name } = req.body
+  if (
+    scheme_name === undefined || 
+    typeof scheme_name !== 'string' ||
+    !scheme_name.trim()
+    ){
+      next({ status: 400, message: 'invalid scheme_name'})
+  } else {
+    next()
+  }
 }
 
 /*
@@ -44,7 +53,20 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
+  
+  const { instructions, step_number} = req.body
+  if (
+    instructions === undefined || 
+    typeof instructions !== 'string' ||
+    !instructions.trim() || 
+    typeof step_number !== 'number' ||
+    step_number < 1
+    ){
+      const err = { status: 400, message: 'invalid step'}
+   next(err)
+  } else {
+    next()
+  }
 }
 
 module.exports = {
